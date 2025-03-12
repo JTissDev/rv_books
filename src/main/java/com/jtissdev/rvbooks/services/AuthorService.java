616efa3d117783473481b00e4259d3c4ea8c23d6
J.Tissier <jtdevweb@gmail.com>
2025-03-12 08:52:56 +0100

    Services files created

---
+++ src/main/java/com/jtissdev/rvbooks/services/AuthorService.java
@@ -0,0 +1,36 @@
+package com.jtissdev.rvbooks.services;
+
+import com.jtissdev.rvbooks.models.Author;
+import com.jtissdev.rvbooks.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author addAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public Author getAuthorById(int id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public Author updateAuthor(int id, Author author) {
+        author.setId(id);
+        return authorRepository.save(author);
+    }
+
+    public void deleteAuthor(int id) {
+        authorRepository.deleteById(id);
+    }
+}
