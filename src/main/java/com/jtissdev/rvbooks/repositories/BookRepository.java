67553ed5056f69a10853c876bd41680054458c66
J.Tissier <jtdevweb@gmail.com>
2025-03-12 09:09:28 +0100

    Search books request

--- src/main/java/com/jtissdev/rvbooks/repositories/BookRepository.java
+++ src/main/java/com/jtissdev/rvbooks/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.jtissdev.rvbooks.repositories;
 
 import com.jtissdev.rvbooks.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,7 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     List<Book> findByPublishersNameContaining(String publisher);
     List<Book> findByTitleContaining(String title);
     List<Book> findBySeriesContaining(String series);
+     @Query("SELECT b FROM Book b WHERE (:status IS NULL OR b.status = :status) AND (:author IS NULL OR b.author = :author) AND (:publisher IS NULL OR b.publisher = :publisher) AND (:title IS NULL OR b.title = :title) AND (:series IS NULL OR b.series = :series)")
+    List<Book> searchBooks(@Param("status") String status, @Param("author") String author, @Param("publisher") String publisher, @Param("title") String title, @Param("series") String series);
+
 }
