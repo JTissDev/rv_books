cdb347e7a7dc849b8cc5a2a8fbc3460639e608f7
J.Tissier <jtdevweb@gmail.com>
2025-03-29 10:19:47 +0100

    Bug Fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css'; // Assurez-vous que ce chemin est correct
+import App from './App.jsx'; // Ajoutez l'extension .jsx
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
