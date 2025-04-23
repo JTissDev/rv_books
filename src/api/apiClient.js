7dc6f1f50b9199f8f0dc1cbd5e25e832fa4a2f87
J.Tissier <jtdevweb@gmail.com>
2025-04-23 09:25:07 +0200

    Request now have body

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,8 +1,47 @@
 import config from '../config';
 
 const apiClient = {
-    get: async (url) => {
-        const response = await fetch(`${config.apiBaseUrl}${url}`);
+    get: async (url, options = {}) => {
+        const response = await fetch(`${config.apiBaseUrl}${url}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', ...options.headers },
+            ...options,
+        });
+        if (!response.ok) {
+            throw new Error(`Erreur lors de la requête : ${response.statusText}`);
+        }
+        return await response.json();
+    },
+    post: async (url, body, options = {}) => {
+        const response = await fetch(`${config.apiBaseUrl}${url}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', ...options.headers },
+            body: JSON.stringify(body),
+            ...options,
+        });
+        if (!response.ok) {
+            throw new Error(`Erreur lors de la requête : ${response.statusText}`);
+        }
+        return await response.json();
+    },
+    put: async (url, body, options = {}) => {
+        const response = await fetch(`${config.apiBaseUrl}${url}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', ...options.headers },
+            body: JSON.stringify(body),
+            ...options,
+        });
+        if (!response.ok) {
+            throw new Error(`Erreur lors de la requête : ${response.statusText}`);
+        }
+        return await response.json();
+    },
+    delete: async (url, options = {}) => {
+        const response = await fetch(`${config.apiBaseUrl}${url}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json', ...options.headers },
+            ...options,
+        });
         if (!response.ok) {
             throw new Error(`Erreur lors de la requête : ${response.statusText}`);
         }
